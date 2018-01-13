8168a3134f2a229c0a11461f99c436b3b353ee95
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-13 15:05:45 +0300

    display all product on home page index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
 	def index
-					
+		@items = Item.all					
 	end	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.delete_all
+Item.create! id: 1, name: "Item 1", price: 49
+Item.create! id: 2, name: "Item 2", price: 29
+Item.create! id: 3, name: "Item 3", price: 99
