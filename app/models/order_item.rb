93b654d9b8f7cde35bd29edf33777909aca67b10
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-13 14:32:48 +0300

    model OrderItem: validate item_order quantity, calculate item total_price

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,29 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    
+  def total_price
+  	item.price * quantity
+  end
+
+  private
+
+  def item_present
+    if item.nil?
+      errors.add(:item, "is not valid.")
+    end
+  end
+
+  def order_present
+    if order.nil?
+      errors.add(:order, "is not a valid order.")
+    end
+  end
+
+  def finialize
+  	self[:total_price] = total_price
+  end
+
 end
